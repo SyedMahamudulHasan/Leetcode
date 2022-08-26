b43a694a2992407293c649b5565e859153526218
omedia23 <syedhasan.cse@gmail.com>
2022-08-26 12:51:49 +0600

    dart solution

--- 2 sum.dart
+++ 2 sum.dart
@@ -12,3 +12,20 @@ class Solution {
 
   }
 }
+
+
+///solution 2
+///Runtime: 484 ms, faster than 72.63% of Dart online submissions for Contains Duplicate.
+///Memory Usage: 167.6 MB, less than 82.11% of Dart online submissions for Contains Duplicate.
+
+class Solution {
+  bool containsDuplicate(List<int> nums) {
+      nums.sort();
+      for(int i=0; i < nums.length-1; i++){
+          if(nums[i] == nums[i+1]) 
+              return true;
+      }
+      return false;
+
+  }
+}
